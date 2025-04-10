98f43b2b27fed496270a82a0a4726f77ce76ff9b
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2025-04-10 18:42:59 +0300

    fix-s-hw

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,20 +4,7 @@ import img from '../../assets/images/s15.png';
 function Layout({ children }) {
   return (
     <main className={css.app}>
-      <img
-        src={img}
-        alt="telephone"
-        style={{
-          height: '640px',
-          width: '680px',
-          position: ' absolute;',
-
-          zIndex: '-1',
-          objectFit: 'cover',
-          filter: 'blur(100px)',
-          opacity: '0.3',
-        }}
-      />
+      <img src={img} alt="telephone" className={css.images} />
       <div className={css.contentWrapper}>{children}</div>
     </main>
   );
